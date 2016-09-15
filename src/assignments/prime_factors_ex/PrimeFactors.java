1ddf41204fb72b5bdc5c137ea68e05e6caeb19a3
rachelsmithcode <Rachelcodesmith@outlook.com>
2016-09-15 18:46:12 +0100

    files created and first test green, for a blank return when n = 1

---
+++ src/assignments/prime_factors_ex/PrimeFactors.java
@@ -0,0 +1,13 @@
+package assignments.prime_factors_ex;
+
+/**
+ * Created by RachelSmith on 15/09/2016.
+ */
+public class PrimeFactors {
+
+    public void generate(int n) {
+        System.out.println();
+
+
+    }
+}
---
+++ tests/assignments/prime_factors_ex/PrimeFactorsTests.java
@@ -0,0 +1,37 @@
+package assignments.prime_factors_ex;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by RachelSmith on 15/09/2016.
+ */
+public class PrimeFactorsTests {
+
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void printsABlankListFor1() throws Exception {
+        new PrimeFactors().generate(1);
+        assertEquals("\n", outContent.toString());
+    }
+
+
+}
